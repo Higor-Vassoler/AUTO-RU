42cc0a3f54e981cfdc7ecf875c264c7d26b9a5ec
Higor Vassoler <vassolerhigorpelo@gmail.com>
2026-05-21 22:47:34 -0300

    Atualizei o manejo das rotas no arquivo main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './index.css'
 import Home from './pages/home/index.jsx'
+import Cadastro from './pages/cadastro/index.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <BrouserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrouserRouter>
   </StrictMode>,
 )
