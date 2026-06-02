b055f91b8fe1fe130614b1561b32e09b7a10cc3b
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-06-02 15:06:08 -0300

    chore: comenta a implementacao temporariamente para testes

---
+++ front-end/src/pages/cadastro-produtos/index.jsx
@@ -0,0 +1,114 @@
+// import { useState } from "react";
+// import "./style.css";
+
+// function Home() {
+
+
+//   return (
+//     <>
+//       <body>
+//         <form>
+//           <fieldset>
+//             <div class="topo">
+//               <legend class="titulo">Dados Basicos</legend>
+//               <p>Preencha as informações para cadastrar ou editar o produto.</p>
+//             </div>
+
+//             <div class="coluna_pai">
+//               <div class="coluna_filha">
+//                 <h2>Identificação</h2>
+
+//                 <div class="campo">
+//                   <label for="id_produto">Código/Id:</label>
+//                   <input
+//                     type="text"
+//                     id="id_produto"
+//                     name="id"
+//                     pattern="[0-9]{13}"
+//                     placeholder="789 123456 789 0"
+//                     required
+//                   />
+//                 </div>
+
+//                 <div class="campo">
+//                   <label for="nome">Nome:</label>
+//                   <input
+//                     type="text"
+//                     id="nome"
+//                     name="nome"
+//                     placeholder="Nome do produto"
+//                     required
+//                   />
+//                 </div>
+
+//                 <div class="campo">
+//                   <label for="venda">Preço de Venda:</label>
+//                   <input
+//                     type="text"
+//                     id="venda"
+//                     name="venda"
+//                     inputmode="decimal"
+//                     pattern="[0-9]+,[0-9]{2}"
+//                     placeholder="R$0,00"
+//                     required
+//                   />
+//                   <small>Formato: 0,00</small>
+//                 </div>
+//               </div>
+
+//               <div class="coluna_filha">
+//                 <h2>Detalhes</h2>
+
+//                 <div class="campo">
+//                   <label for="categoria">Categoria</label>
+//                   <select id="categoria" name="categoria" required>
+//                     <option value="">Selecione uma opção</option>
+//                     <option value="bebida">Bebida</option>
+//                     <option value="comida">Comida</option>
+//                     <option value="doces">Doces</option>
+//                     <option value="ficha">Ficha</option>
+//                   </select>
+//                 </div>
+
+//                 <div class="campo">
+//                   <label for="estoque">Quantidade</label>
+//                   <input
+//                     type="number"
+//                     id="estoque"
+//                     name="estoque"
+//                     min="0"
+//                     placeholder="Quantidade no estoque"
+//                     required
+//                   />
+//                 </div>
+
+//                 <div class="campo">
+//                   <label for="descricao">Descrição</label>
+//                   <textarea
+//                     id="descricao"
+//                     name="descricao"
+//                     placeholder="Escreva aqui"
+//                     required
+//                   ></textarea>
+//                 </div>
+//               </div>
+//             </div>
+
+//             <div class="botoes">
+//               <button type="reset" id="cancelar">
+//                 Cancelar
+//               </button>
+//               <button type="submit" id="salvar" name="salvar" value="salvo">
+//                 Salvar
+//               </button>
+//             </div>
+//           </fieldset>
+//         </form>
+
+//         <script src="script.js"></script>
+//       </body>
+//     </>
+//   );
+// }
+
+// export default Home;
---
+++ front-end/src/pages/cadastro-produtos/script.js
@@ -0,0 +1,10 @@
+// const form = document.querySelector("form");
+
+// form.addEventListener("submit", (event) => {
+//   event.preventDefault(); // Impede a página de recarregar
+
+//   // Mostra um aviso de sucesso na tela
+//   alert("Produto salvo com sucesso!");
+
+//   form.reset(); // Limpa o formulário após salvar
+// });
