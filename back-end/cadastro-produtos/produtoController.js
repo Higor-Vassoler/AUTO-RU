413076e576e40fab6226e3f70b7c8af67092eccb
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-06-02 15:25:25 -0300

    feat: implementa o back-end com a arquitetura MVC

---
+++ back-end/cadastro-produtos/produtoController.js
@@ -0,0 +1,34 @@
+const ProdutoModel = require("./produtoModel");
+
+const ProdutoController = {
+  cadastrar: async (req, res) => {
+    try {
+      const { id, nome, venda, estoque, descricao } = req.body;
+
+      // Tratamento simples: converte a vírgula do preço em ponto para o MySQL
+      const precoFormatado = parseFloat(venda.replace(",", "."));
+
+      const novoProduto = {
+        id_produto: id,
+        nome: nome,
+        descricao: descricao,
+        preco_unitario: precoFormatado,
+        quantidade_estoque: parseInt(estoque),
+      };
+
+      await ProdutoModel.salvar(novoProduto);
+
+      // Responde ao React com sucesso
+      return res
+        .status(201)
+        .json({ mensagem: "Produto cadastrado com sucesso!" });
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ erro: "Erro ao salvar o produto no banco de dados." });
+    }
+  },
+};
+
+module.exports = ProdutoController;
