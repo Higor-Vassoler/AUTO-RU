7f8f3c968b0b7039786a2bc59182d46078600323
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-06-19 15:11:17 -0300

    fix: corrigir caminhos de importação dos componentes no layout

--- front-end/src/components/layout/layout.jsx
+++ front-end/src/components/layout/layout.jsx
@@ -1,6 +1,6 @@
-import Header from "./components/header/header.jsx";
-import Sidebar from "./components/sidebar/sidebar.jsx";
-import Footer from "./components/footer/footer.jsx";
+import Header from "../header/header.jsx";
+import Sidebar from "../sidebar/sidebar.jsx";
+import Footer from "../footer/footer.jsx";
 import "./layout.css";
 
 export default function Layout({ children }) {
