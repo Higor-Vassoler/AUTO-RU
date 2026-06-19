11039512a065d7e3486275a3288ee521226c47c5
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-06-19 13:01:00 -0300

    refactor: adicionar componente Header.jsx para melhor organização do projeto

---
+++ front-end/src/components/header/header.jsx
@@ -0,0 +1,35 @@
+import "./Header.css";
+import { ShoppingCart, UserCircle, UtensilsCrossed } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <div className="header__logo">
+        <div className="logo-circle">
+          <UtensilsCrossed size={28} />
+        </div>
+
+        <div className="logo-text">
+          <h1>RU</h1>
+          <span>Refeitório Universitário</span>
+        </div>
+      </div>
+
+      <nav className="header__nav">
+        <a href="/">Catálogo</a>
+        <a href="/">Cardápio</a>
+        <a href="/">Sobre o RU</a>
+        <a href="/">Contato</a>
+      </nav>
+
+      <div className="header__actions">
+        <div className="cart">
+          <ShoppingCart size={28} />
+          <span className="cart-badge">2</span>
+        </div>
+
+        <UserCircle size={42} />
+      </div>
+    </header>
+  );
+}
