7f9c9eb7606fe4b319cc605ab5e960275203b440
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-06-19 15:02:07 -0300

    feat: criar componente Produtos para exibir informações e produtos salvos

---
+++ front-end/src/pages/cadastro-produtos/produtos.jsx
@@ -0,0 +1,20 @@
+import Layout from "../components/layout/layout.jsx";
+
+import InformacoesProduto from "./informacoes-produto.jsx";
+import ProdutosSalvos from "./produtos-salvos.jsx";
+
+{
+  /* Implementar logica de backend para injetar os valores de "produtos" do banco de dados na tabela do arquivo "produtos-salvos.jsx" */
+}
+
+export default function Produtos() {
+  const produtos = []; {/* <- Talvez tenha q mudar isso aq */}
+
+  return (
+    <Layout>
+      <InformacoesProduto />
+
+      <ProdutosSalvos produtos={produtos} />
+    </Layout>
+  );
+}
