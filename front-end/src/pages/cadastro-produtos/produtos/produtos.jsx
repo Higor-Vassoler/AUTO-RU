67185f57e30c31f3505ac59944506d82c1cff18b
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-06-21 12:36:24 -0300

    style: criar componente Produtos para o formulário de cadastro de produtos

---
+++ front-end/src/pages/cadastro-produtos/produtos/produtos.jsx
@@ -0,0 +1,30 @@
+import Layout from "../../components/layout/layout.jsx";
+import InformacoesProduto from "./informacoes-produto.jsx";
+import ProdutosSalvos from "./produtos-salvos.jsx";
+
+{
+  /* Implementar logica de backend para injetar os valores de "produtos" do banco de dados na tabela do arquivo "produtos-salvos.jsx" */
+}
+
+export default function Produtos() {
+  const produtos = [];
+  {
+    /* <- Talvez tenha q mudar isso aq, parte do backend */
+  }
+
+  return (
+    <Layout>
+      <div className="page-header">
+        <h1>Cadastrar produto</h1>
+        <p>
+          Preencha as informações abaixo para adicionar um novo produto ao
+          catálogo.
+        </p>
+      </div>
+
+      <InformacoesProduto />
+
+      <ProdutosSalvos produtos={produtos} />
+    </Layout>
+  );
+}
