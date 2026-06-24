623c3acdca1203d5146deb99a863c4530a196a45
Higor Vassoler <vassolerhigorpelo@gmail.com>
2026-06-24 14:57:57 -0300

    Fix: Erro de importação no server do backend.

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import sequelize from './config/database.js';
-import "./models/index.js"
-
 
 const app = express();
 const PORT = process.env.PORT || 5000;
