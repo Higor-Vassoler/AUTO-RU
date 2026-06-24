eaa01802fc1be79f9013e6f9901f86e4f82e9f69
Higor Vassoler <vassolerhigorpelo@gmail.com>
2026-06-24 14:53:12 -0300

    Refactor: Removi uma pasta auxiliar.

--- BACKFUTURO/routes/ProdutoRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { criarProduto, listarProdutos } from "../controllers/ProdutoController.js";
-
-const router = express.Router();
-
-router.post("/produtos", criarProduto);
-router.get("/produtos", listarProdutos);
-
-export default router;
