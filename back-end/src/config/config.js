0e0200fa0e21e30bdd269b12b79214f235a96765
ptrassi <ptrassi@alunos.utfpr.edu.br>
2026-06-29 17:54:27 -0300

    Fix: renomeia DB_PASSWORD para DB_PASS em .env.example e config.js

--- back-end/src/config/config.js
+++ back-end/src/config/config.js
@@ -10,7 +10,7 @@ export default {
     development: {
         database: process.env.DB_NAME,
         username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
+        password: process.env.DB_PASS,
         host: process.env.DB_HOST,
         dialect: 'mysql'
     }
