d564ee5b3f857b28244a81ea0812be489d659769
linux1234 <joaomanoelferreira@utfpr.edu.br>
2026-07-02 23:55:04 -0300

    Refactor: apaguei o espaço que estava dando erro no pedido.cjs

--- back-end/src/migrations/20260628060500-pedido.cjs
+++ back-end/src/migrations/20260628060500-pedido.cjs
@@ -4,40 +4,32 @@
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.createTable('pedidos', {
-        id: {
-          type: Sequelize.INTEGER,
-          primaryKey: true,
-          autoIncrement: true,
-          allowNull: false
-        },
-        id_usuario: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'Usuarios',
-            key: 'id'
-          }
-        },
-        id_forma_pagamento: {
-          type: Sequelize.INTEGER,
-          allowNull: false,
-          references: {
-            model: 'Forma_de_Pagamentos',
-            key: 'id_forma_de_pagamento'
-          }
-        },
-        data: {
-          type: Sequelize.DATE,
-          allowNull: false,
-          defaultValue: Sequelize.NOW
-        },
-        preco_total: {
-          type: Sequelize.DECIMAL(10, 2),
-          allowNull: false
-        },
-         createdAt: {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      id_usuario: {
+        type: Sequelize.INTEGER,
         allowNull: false,
-        type: Sequelize.DATE
+        references: {
+          model: 'Usuarios',
+          key: 'id'
+        }
+      },
+      data: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW
+      },
+      preco_total: {
+        type: Sequelize.DECIMAL(10, 2),
+        allowNull: false
+      },
+        createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
