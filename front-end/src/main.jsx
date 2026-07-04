dfa475509400b8a917a0ceb4cdd9a63f83ab718d
Higor Vassoler <vassolerhigorpelo@gmail.com>
2026-07-04 18:22:47 -0300

    Fix: Pequeno erro no código da main.jsx

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -18,15 +18,16 @@ import MinhasCompras from "./pages/minhas-compras/minhas-compras.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* 🟢 ROTAS PÚBLICAS */}
-        <Route path="/" element={<Login />} />
-        <Route path="/catalogo" element={<Catalogo />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/sobre" element={<SobreRU />} />
-        <Route path="/cardapio" element={<CardapioRU />} />
-        <Route path="/contato" element={<ContatoRU />} />
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          {/* 🟢 ROTAS PÚBLICAS */}
+          <Route path="/" element={<Login />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/sobre" element={<SobreRU />} />
+          <Route path="/cardapio" element={<CardapioRU />} />
+          <Route path="/contato" element={<ContatoRU />} />
 
           {/* 🔴 ROTAS PRIVADAS */}
           <Route
@@ -56,5 +57,5 @@ createRoot(document.getElementById("root")).render(
         </Routes>
       </BrowserRouter>
     </CartProvider>
-  </StrictMode>,
+  </StrictMode >,
 );
