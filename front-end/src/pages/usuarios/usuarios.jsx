63fc3022d4bf9420ee0ad18d58cdc6d772372f34
Eduardo Cordeiro Pedrozo <EdCordeiro.pedrozo@gmail.com>
2026-07-06 17:21:16 -0300

    Feat: adiciono opção de pesquisa funcional.

--- front-end/src/pages/usuarios/usuarios.jsx
+++ front-end/src/pages/usuarios/usuarios.jsx
@@ -86,7 +86,7 @@ export default function Usuarios() {
             <input
               type="text"
               className="usuarios-search-input"
-              placeholder="Pesquisar usuários por nome, e-mail ou matrícula..."
+              placeholder="Pesquisar usuários por nome, e-mail ou matrícula"
               value={busca}
               onChange={handleBuscar}
             />
